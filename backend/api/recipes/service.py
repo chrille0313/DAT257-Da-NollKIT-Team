2cf133b0423586795c1242038af9df8e61e4c341
N0_L0gic <chrille_0313@hotmail.com>
2024-04-12 13:24:25 +0200

    Update comments
    Co-Authored-By: Ida Åberg <117651596+Idaaberg@users.noreply.github.com>
    Co-Authored-By: Mårten Granath <115485279+Marten-Granath@users.noreply.github.com>
    Co-Authored-By: Erik Dreifeldt <94061173+erikdreifeldt1@users.noreply.github.com>

--- backend/api/recipes/service.py
+++ backend/api/recipes/service.py
@@ -7,14 +7,14 @@ class RecipeService:
         url = 'https://api.edamam.com/api/recipes/v2'
 
         params = {
-            'app_id': '94cb00ae',
-            'app_key': '8dfe8940a9a85b83edfa9e7b97f4e5b0',
+            'app_id': '94cb00ae',  # FIXME: regenerate and use environment variable
+            'app_key': '8dfe8940a9a85b83edfa9e7b97f4e5b0',  # FIXME: regenerate and use environment variable
             'type': 'any',
             'q': 'salt',
             'mealType': 'Dinner',
             'dishType': 'Main course',
             'random': 'true',
-            'field': ['label', 'calories', 'image', 'co2EmissionsClass', 'yield'] # , 'image'
+            'field': ['label', 'calories', 'image', 'co2EmissionsClass', 'yield']
         }
 
         response = requests.get(url, params=params)
