abece127796f4dae05823d5f22659d43dc737fdc
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2024-04-12 12:40:47 +0200

    Removed uneccessary comments

--- backend/api/queries.py
+++ backend/api/queries.py
@@ -1,10 +1,5 @@
 import requests
 
-# Titel
-# Bild
-# Yields
-# Calories
-# KoldioxidKlass
 request_get = requests.get('https://api.edamam.com/api/recipes/v2?type=any&q=salt&app_id=94cb00ae&app_key=8dfe8940a9a85b83edfa9e7b97f4e5b0&mealType=Dinner&dishType=Main%20course&random=true&field=label&field=calories&field=image&field=co2EmissionsClass&field=yield', auth=('user', 'pass'))
 request_data = request_get.json()['hits']
 for x in range(5):
@@ -15,9 +10,3 @@ for x in range(5):
     print(f"{request_data[x]["recipe"]["calories"]} Calories")
     print(f"{request_data[x]["recipe"]["co2EmissionsClass"]} CO2 Emissions class (A-G)")
     print()
-#for hit_number in range(5):
-    #print(request_data[hit_number])
-    #print(request_data[hit_number].image)
-    #print(request_data[hit_number].yields)
-    #print(request_data[hit_number].calories)
-    #print(request_data[hit_number].totalCO2Emissions)
