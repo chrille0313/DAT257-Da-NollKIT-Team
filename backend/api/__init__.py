01e205ac09d60013733329c95a3a9396b7370ad7
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2024-04-18 23:21:57 +0200

    Added params to controller
    Co-Authored-By: Ida Åberg <117651596+Idaaberg@users.noreply.github.com>
    Co-Authored-By: Christian Mattsson <47303717+chrille0313@users.noreply.github.com>
    Co-Authored-By: Knodeln <112890904+Knodeln@users.noreply.github.com>

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -4,3 +4,12 @@ LATEST_API_VERSION = 'v1'
 
 def get_base_api_route(version=None):
     return API_BASE_ROUTE + LATEST_API_VERSION if version is None else version
+
+
+def normalize_query_param(value):
+    return value[0] if len(value) == 1 else value
+
+
+def normalize_query_params(params):
+    params_non_flat = params.to_dict(flat=False)
+    return {k: normalize_query_param(v) for k, v in params_non_flat.items()}
--- backend/api/recipes/routes.py
+++ backend/api/recipes/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from api import get_base_api_route
+from flask import Blueprint, jsonify, request
+from api import get_base_api_route, normalize_query_params
 from api.recipes.service import RecipeService  # TODO: Fix better relative import
 
 recipe_routes = Blueprint('recipe_routes', __name__)
@@ -8,4 +8,4 @@ recipe_service = RecipeService()
 
 @recipe_routes.route(f"{get_base_api_route()}/recipes")
 def get():
-    return jsonify(recipe_service.get_random_recipes())
+    return jsonify(recipe_service.get_random_recipes(**normalize_query_params(request.args)))
