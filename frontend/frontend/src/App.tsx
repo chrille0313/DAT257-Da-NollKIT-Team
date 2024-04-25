87215dc04f7c43dffd15d4f0877749a88cfc7ee2
N0_L0gic <chrille_0313@hotmail.com>
2024-04-25 17:56:26 +0200

    Delete logo.svg

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RecipeColumnContainer from './components/RecipeColumnContainer';
 import Navbar from './components/Navbar';
