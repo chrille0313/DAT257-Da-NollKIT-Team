36bfa4fdd3f93969a7451540c030926ec07d9ded
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2024-04-29 16:42:01 +0200

    Changed location of EmissionClass and added Emissionbar

--- frontend/frontend/src/components/RecipeColumn/RecipeColumn.tsx
+++ frontend/frontend/src/components/RecipeColumn/RecipeColumn.tsx
@@ -2,24 +2,38 @@ import styles from './RecipeColumn.module.css';
 import kangaroo from '../../static/images/kangaroo.jpg';
 import kangaroo2 from '../../static/images/kram-med-ru.jpg';
 import { Recipe } from '../../types';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 interface RecipeColumnProps {
   recipe: Recipe
 }
 
 function RecipeColumn({recipe}: RecipeColumnProps) {
+  
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <article className={styles.RecipeContainer}>
-      
       <div className={styles.RecipeImageContainer}> 
         
         <img src={recipe.image} alt='Tasty kangaroo meat'></img>
-        <div className = {styles.EmissionContainer}>
-          <div className={styles.EmissionsClass}>
-            <p>{recipe.co2EmissionsClass} </p>
-          </div>
-        </div>
         <div className={styles.ImageTextOverlay}>
           <p>{recipe.totalTime} min</p>
           <p>{recipe.yield} portions</p>
@@ -27,20 +41,19 @@ function RecipeColumn({recipe}: RecipeColumnProps) {
       </div>
 
       <div className={styles.RecipeInfoContainer}>
-        <h2 className={styles.RecipeTitle}>{recipe.label}</h2>
-       {/* <hr /> */}
-        {/* <div className={styles.IngredientsContainer}>
-          <h3 className={styles.RecipeSubtitle}>Ingredients</h3>
-
-          <table className={styles.IngredientsTable}>
-            {recipe.ingredients.map((ingredient, index) => (
-              <tr key={index}>
-                <td>{ingredient.food}</td>
-                <td>{ingredient.quantity} {ingredient.measure}</td>
-              </tr>
-            ))}
-          </table>
-        </div> */}
+        <div className={styles.RecipeTitleContainer}>
+          <h2 className={styles.RecipeTitle}>{recipe.label}</h2>
+        </div>
+        <div className = {styles.EmissionContainer}>
+          <div className={styles.EmissionsClass}>
+            <p>{recipe.co2EmissionsClass} </p>
+          </div>
+          
+            <Box sx={{ width: '80%' }}>
+              <LinearProgress variant="determinate" value={progress} />
+            </Box>
+        
+        </div>
       </div>
     </article>
   );
