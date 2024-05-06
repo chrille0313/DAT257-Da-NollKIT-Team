93846c1f8422a98701a8fad59a23a225cc34748c
N0_L0gic <chrille_0313@hotmail.com>
2024-05-06 10:49:33 +0200

    Update Responsive.tsx

--- frontend/frontend/src/components/Responsive/Responsive.tsx
+++ frontend/frontend/src/components/Responsive/Responsive.tsx
@@ -4,27 +4,28 @@ interface ResponsiveProps {
   children: React.ReactNode
 }
 
+export const useDesktop = () => useMediaQuery({ minWidth: 992 });
+export const useTablet = () => useMediaQuery({ minWidth: 768, maxWidth: 991 });
+export const useTabletAndBelow = () => useMediaQuery({ maxWidth: 991 });
+export const useMobile = () => useMediaQuery({ maxWidth: 767 });
+export const useNotMobile = () => useMediaQuery({ minWidth: 768 });
+
 export const Desktop = ({ children }: ResponsiveProps) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 })
-  return isDesktop ? <>{children}</> : null
+  return useDesktop() ? <>{children}</> : null
 }
 
 export const Tablet = ({ children }: ResponsiveProps) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-  return isTablet ? <>{children}</> : null
+  return useTablet() ? <>{children}</> : null
 }
 
 export const TabletAndBelow = ({ children }: ResponsiveProps) => {
-  const isTabletAndBelow = useMediaQuery({ maxWidth: 991 })
-  return isTabletAndBelow ? <>{children}</> : null
+  return useTabletAndBelow() ? <>{children}</> : null
 }
 
 export const Mobile = ({ children }: ResponsiveProps) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 })
-  return isMobile ? <>{children}</> : null
+  return useMobile() ? <>{children}</> : null
 }
 
 export const Default = ({ children }: ResponsiveProps) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 })
-  return isNotMobile ? <>{children}</> : null
+  return useNotMobile() ? <>{children}</> : null
 }
