a30467ac597f755a73da7a4cc1576a007b381e09
Christian <chrille_0313@hotmail.com>
2024-05-14 14:17:43 +0200

    Include filters in api query

--- frontend/frontend/src/components/RecipeColumnContainer/RecipeColumnContainer.tsx
+++ frontend/frontend/src/components/RecipeColumnContainer/RecipeColumnContainer.tsx
@@ -3,20 +3,20 @@ import RecipeColumn from "../RecipeColumn/RecipeColumn";
 import styles from './RecipeColumnContainer.module.css';
 import { get } from '../../utils';
 import { RecipeAPIResponse, Recipe } from '../../types';
+import { useAppSelector } from "../../app/hooks";
 
 
 const api_url = "http://127.0.0.1:5000/api/v1";
 const recipe_url = api_url + "/recipes";
 
 function RecipeColumnContainer() {
+  const filters = useAppSelector(state => state.api.filters);
   const [recipes, setRecipes] = useState<Recipe[]>([]);
 
   async function fetchData() {
-    const response = await get<RecipeAPIResponse>(recipe_url);
-    console.log(response)
+    const response = await get<RecipeAPIResponse>(recipe_url, filters);
     const responseRecipes = response.map((recipeResponse) => recipeResponse.recipe);
-    //console.log(responseRecipes);
-    setRecipes(responseRecipes)
+    setRecipes(responseRecipes);
   };
 
   useEffect(() => {
