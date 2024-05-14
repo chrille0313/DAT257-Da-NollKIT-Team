fdc313837be787923708976e13991dd2cdb78cfa
Christian <chrille_0313@hotmail.com>
2024-05-14 14:17:14 +0200

    add params to get request

--- frontend/frontend/src/utils/Fetchers.tsx
+++ frontend/frontend/src/utils/Fetchers.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
-
-export async function get<T>(path: string): Promise<T> {
-    const response = await axios.get(path);
+export async function get<T>(url: string, params?: AxiosRequestConfig['params']): Promise<T> {
+    const response = await axios.get(url, { params });
     return response.data;
 }
