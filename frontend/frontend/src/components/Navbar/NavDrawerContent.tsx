782a0da3398891c1cdbd1656c587f90fe78db0b3
Christian <chrille_0313@hotmail.com>
2024-05-14 03:44:17 +0200

    Refactor

--- frontend/frontend/src/components/Navbar/NavDrawerContent.tsx
+++ frontend/frontend/src/components/Navbar/NavDrawerContent.tsx
@@ -3,31 +3,31 @@ import styles from './Navbar.module.css';
 import FilterDropdown, { DropdownOption, FilterDropdownProps } from "../FilterDropdown";
 import { RecipeFilters, RecipeFilterName  } from "../../types";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import filters from "../../features/api/FilterSlices";
+import filterSlices from "../../features/api/FilterSlices";
+
+
+const FilterDropdownContainer = ({filterName}: {filterName: RecipeFilterName}) => {
+  const dispatch = useAppDispatch();
+  const filterProps = RecipeFilters[filterName];
+  const filterValue = useAppSelector(state => state.api.filters[filterName]);
+  const setFilter = filterSlices[filterName].actions.setFilter;
+
+  return (
+    <FilterDropdown
+      {...filterProps}
+      value={filterValue}
+      onChange={(event, newValue) => dispatch(setFilter(newValue))}
+    />
+  );
+}
 
 export default function NavDrawerContent() {
-  const dispatch = useAppDispatch();
-  const allFilters = useAppSelector(state => state.api.filters);
-
-  const createFilterDropdown = (filterName: RecipeFilterName) => {
-    const filterProps = RecipeFilters[filterName];
-    const filterValue = allFilters[filterName];
-    const setFilter = filters[filterName].actions.setFilter;
-
-    return (
-      <FilterDropdown
-        key={filterName}
-        {...filterProps}
-        value={filterValue}
-        onChange={(event, newValue) => dispatch(setFilter(newValue))}
-      />
-    );
-  };
-
   return (
     <Box role="presentation" className={styles.NavDrawerContent}>
       {
-        (Object.keys(RecipeFilters) as RecipeFilterName[]).map(createFilterDropdown)
+        (Object.keys(RecipeFilters) as RecipeFilterName[]).map(filterName =>
+          <FilterDropdownContainer key={filterName} filterName={filterName} />
+        )
       }
       {/* <RangedSlider 
         value={timeRange}
