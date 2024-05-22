a3437d5d4d32c19cae84ceb65d0d75ad48ebf959
N0_L0gic <chrille_0313@hotmail.com>
2024-05-22 12:07:41 +0200

    Update CustomModal

--- frontend/frontend/src/components/CustomModal/CustomModal.tsx
+++ frontend/frontend/src/components/CustomModal/CustomModal.tsx
@@ -1,60 +1,56 @@
-// src/components/CustomModal.tsx
-import React, { ReactNode } from 'react';
-import { Modal, Box, Button } from '@mui/material';
 import styles from './CustomModal.module.css';
+import { PropsWithChildren } from 'react';
+import { CloseRounded } from '@mui/icons-material';
+import { Modal, Box, IconButton, Divider } from '@mui/material';
 import { Recipe } from '../../types'; 
+import { Button } from '@mui/material';
+
 
 interface CustomModalProps {
   open: boolean;
   onClose: () => void;
-  children: ReactNode; // Declare the children prop
   recipe: Recipe;
 }
 
-export default function CustomModal ({ open, onClose, children, recipe }: CustomModalProps) {
+export default function CustomModal ({ open, onClose, children, recipe }: PropsWithChildren<CustomModalProps>) {
   return (
-    <Modal className={styles.modalBackground} open={open} onClose={onClose}>
-      <Box className={styles.infoContainer}>
-        <div className={styles.InfoContainerSplit}>
-           {/* TODO: Carousel for multiple images */}
-          <img className = {styles.FoodImage} src={recipe.image} alt='Picture of food'></img> 
-        </div>
-        <div className={styles.InfoContainerSplit}>
+    <Modal className={styles.ModalContainer} open={open} onClose={onClose}>
+      <Box className={styles.Modal}>
+        {/* TODO: Carousel for multiple images */}
+        <img className = {styles.FoodImage} src={recipe.image} alt='Picture of food' /> 
+        
+        <section className={styles.RecipeContentContainer}>
           <div className = {styles.NavbarContainer}>
-            <div className={styles.CloseButtonContainer}>
-              <Button onClick={onClose} className={styles.closeButton}>
-                <div className={styles.closeButtonText} >
-                  X
-                </div>        
-              </Button>
+            <IconButton onClick={onClose} className={styles.closeButton}>
+              <CloseRounded />
+            </IconButton>
+          </div>
+          <section className={styles.RecipeContent}>
+            <header className={styles.RecipeTitle}>
+              <a className={styles.RecipeLink} href={recipe.url} target="_blank"> 
+                <h1 className={styles.RecipeTitleText}>{recipe.label}</h1>
+              </a>
+            </header>
+            <Divider variant='middle' />
+            <div className ={styles.IngredientsContainer}>
+              <h3 className={styles.RecipeSubtitle}>Ingredients</h3>
+              <table className={styles.IngredientsTable}>
+                {recipe.ingredients.map((ingredient, index) => (
+                  <tr key={index}>
+                    <td>{ingredient.food}</td>
+                    <td>{ingredient.quantity} {ingredient.measure}</td>
+                  </tr>
+                ))}
+              </table>
             </div>
-          </div>
-          <div className =  {styles.RecipeTitle}>
-            <a className = {styles.RecipeLink}  href={recipe.url} target="_blank"> 
-              <h1 className={styles.RecipeTitleText} > {recipe.label}</h1>
-            </a>
-          </div>
-          <div className={styles.RecipeContentSplit}>
-
-              <div className={styles.RecipeContentSplit}>
-              <div className ={styles.Ingredients}>
-                <div className={styles.IngredientsContainer}>
-                <h3 className={styles.RecipeSubtitle}>Ingredients</h3>
-                <table className={styles.IngredientsTable}>
-                  {recipe.ingredients.map((ingredient, index) => (
-                    <tr key={index}>
-                      <td>{ingredient.food}</td>
-                      <td>{ingredient.quantity} {ingredient.measure}</td>
-                    </tr>
-                  ))}
-                </table>
-                </div>
-              </div>
-              </div>      
-          </div>
-
+            <section className={styles.ReadMoreButtonContainer}>
+              <Button className={styles.ReadMoreButton} variant='contained' href={recipe.url}>
+                Read More
+              </Button>
+            </section>
+          </section>
           {children}
-        </div>
+        </section>
       </Box>
     </Modal>
   );
