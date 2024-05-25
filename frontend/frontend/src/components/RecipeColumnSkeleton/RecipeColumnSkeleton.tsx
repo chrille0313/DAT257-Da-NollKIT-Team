971059bdfcad7eacee9fa8227a78fd10c076c66b
Christian <chrille_0313@hotmail.com>
2024-05-25 19:16:50 +0200

    Polish skeletons

--- frontend/frontend/src/components/RecipeColumnSkeleton/RecipeColumnSkeleton.tsx
+++ frontend/frontend/src/components/RecipeColumnSkeleton/RecipeColumnSkeleton.tsx
@@ -1,16 +1,42 @@
 import styles from './RecipeColumnSkeleton.module.css';
-import {Stack, Skeleton, Typography} from '@mui/material';
+import {Stack, Skeleton, Typography, Box} from '@mui/material';
 
 const RecipeColumnSkeleton = () => { 
-    return (
-        <Stack className={styles.container}>
-            <Skeleton className={styles.skeletonImage} variant="rounded" width={300} height={500} />
-            <Typography className={styles.skeletonHeader} variant="h3"><Skeleton /> </Typography>  
-            <Skeleton className={styles.skeletonText} variant="text" sx={{ fontSize: '2rem' }} />  
-            <Skeleton className={styles.skeletonText} variant="text" sx={{ fontSize: '2rem' }} />  
-            <Skeleton className={styles.skeletonText} variant="text" sx={{ fontSize: '2rem' }} />  
+  return (
+    <Stack className={styles.Container}>
+      <Skeleton className={styles.SkeletonImage} variant='rounded' />
+
+      <Stack className={styles.TextContentContainer}>
+        <Stack>
+          <Typography className={styles.IconsContainer} variant='caption'>
+            <Box className={styles.IconWithTextContainer}>
+              <Skeleton variant='circular' width={16} height={16} />
+              <Skeleton width={75} />
+            </Box>
+            <Box className={styles.IconWithTextContainer}>
+              <Skeleton variant='rectangular' width={16} height={16} />
+              <Skeleton width={75} />
+            </Box>
+          </Typography>
+
+          <Typography variant='h3'>
+            <Skeleton /> 
+          </Typography>
+
+          <Typography variant='body1'>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </Typography>
         </Stack>
-    );
+
+        <Box className={styles.CO2Container}>
+          <Skeleton className={styles.CO2Bar} />
+          <Skeleton width={50} />
+        </Box>
+      </Stack>
+    </Stack>
+  );
 }
 
 
